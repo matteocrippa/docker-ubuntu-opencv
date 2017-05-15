0a67fefcc25e110ef35bf1768db991eaa8e68b2b
Matteo Crippa <matteo@boostco.de>
2017-05-15 16:35:30 +0200

    first setup

---
+++ OpenCV.sh
@@ -0,0 +1,11 @@
+cd /root
+unzip 3.2.0.zip
+rm 3.2.0.zip
+mv opencv-3.2.0 OpenCV
+cd OpenCV
+mkdir build
+cd build
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON ..
+make -j4
+make install
+ldconfig
